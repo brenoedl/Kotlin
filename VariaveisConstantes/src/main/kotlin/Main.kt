802a9d45bd7e57d1f97e5aeb594a9fa788b6170d
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-12 16:05:12 -0300

    Variaveis Constantes

---
+++ VariaveisConstantes/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val idade:Short = 21
+    val nome:String = "Breno"
+    val namoro:Boolean = false
+    val peso:Float = 50.25f
+
+    print("Olá " + nome + " você tem " + idade + " anos " +  "e pesa " + peso + "Kg namorando " + namoro)
+
+}
