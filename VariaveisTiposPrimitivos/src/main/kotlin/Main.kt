78e3d71acbb96d32055a75944a9343cd7b5c044c
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-12 15:15:54 -0300

    Variáveis Tipos Primitivos

---
+++ VariaveisTiposPrimitivos/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var idade:Short = 21
+    var nome:String = "Breno"
+    var namoro:Boolean = false
+    var peso:Float = 50.25F
+
+    print("Olá " + nome + " você tem " + idade + " anos " +  "e pesa " + peso + "Kg namorando " + namoro)
+}
