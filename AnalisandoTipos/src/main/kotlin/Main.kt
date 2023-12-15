8cd6485fd6de6d80535be0cd1048807317a3116c
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-15 18:29:59 -0300

    Analisando Tipos

---
+++ AnalisandoTipos/src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+fun main(args: Array<String>) {
+    var nome:String = "Breno"
+
+    println("Byte")
+    println("Minimo: " + Byte.MIN_VALUE)
+    println("Maximo: " + Byte.MAX_VALUE)
+    println("Tamanho: " + Byte.SIZE_BYTES + " bytes\n")
+
+    println("Short")
+    println("Minimo: " + Short.MIN_VALUE)
+    println("Maximo: " + Short.MAX_VALUE)
+    println("Tamanho: " + Short.SIZE_BYTES + " bytes\n")
+
+    println("Int")
+    println("Minimo: " + Int.MIN_VALUE)
+    println("Maximo: " + Int.MAX_VALUE)
+    println("Tamanho: " + Int.SIZE_BYTES + " bytes\n")
+
+    println("Long")
+    println("Minimo: " + Long.MIN_VALUE)
+    println("Maximo: " + Long.MAX_VALUE)
+    println("Tamanho: " + Long.SIZE_BYTES + " bytes\n")
+
+    println("Float")
+    println("Minimo: " + Float.MIN_VALUE)
+    println("Maximo: " + Float.MAX_VALUE)
+    println("Tamanho: " + Float.SIZE_BYTES + " bytes\n")
+
+    println("Double")
+    println("Minimo: " + Double.MIN_VALUE)
+    println("Maximo: " + Double.MAX_VALUE)
+    println("Tamanho: " + Double.SIZE_BYTES + " bytes\n")
+}
