917d50c5159c8347ccc8ba06cbd68fe12e08326a
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-16 10:24:50 -0300

    Valor Nulo

---
+++ ValorNulo/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val x:Any? = null
+    val a:Int? = null
+    val b:Int = a ?: 0
+    println("A variavel x é " + x)
+    println("A variavel a é " + a)
+    println("A variavel b é " + b)
+}
