ec9d619d28282c39089345ae4e3b559d4401d30c
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-17 17:35:08 -0300

    Saídas Interpoladas

---
+++ SaidasInterpoladas/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val anoAtual:Short = 2023
+    val anoNasc:Short = 2002
+    println("""-------------------------------------------
+           Saídas Interpoladas
+-------------------------------------------""")
+    print("Você nasceu em $anoNasc e hoje estamos $anoAtual. Então você tem ${anoAtual - anoNasc} anos...")
+}
