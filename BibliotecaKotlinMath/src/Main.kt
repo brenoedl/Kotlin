45323b721a605eb30bf3f937c2a5ea3cfef988f0
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-21 14:54:59 -0300

    Biblioteca Kotlin.math

---
+++ BibliotecaKotlinMath/src/Main.kt
@@ -0,0 +1,12 @@
+import kotlin.math.*
+
+fun main() {
+    println("Raiz Quadrada: ${sqrt(25f)}")
+    println("Potência: ${7f.pow(2)}")
+    println("Arredonda acima: ${ceil(6.2f)}")
+    println("Arredonda: ${9.5f.roundToInt()}")
+    println("Arredonda abaixo: ${floor(6.8f)}")
+    println("Menor: ${min(2, 9)}")
+    println("Maior: ${max(2, 9)}")
+    print("Absoluto: ${abs(-3)}")
+}
