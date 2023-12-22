b6c22dfadcd4a78ebf352a67bec5269ceb7a267a
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-21 22:07:09 -0300

    Operadores relacionais

---
+++ OperadoresRelacionais/src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val n1:Int = 5
+    val n2:Int = 2
+
+    println("$n1 é naior que $n2? ${n1 > n2}")
+    println("$n1 é menor que $n2? ${n1 < n2}")
+    println("$n1 maior ou igual a $n2? ${n1 >= n2}")
+    println("$n1 menor ou igual a $n2? ${n1 <= n2}")
+    println("$n1 é igual a $n2? ${n1 == n2}")
+    print("$n1 é deferente de $n2? ${n1 != n2}")
+}
