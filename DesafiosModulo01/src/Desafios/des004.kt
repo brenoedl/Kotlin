a5c627f1040c1f05599d14c2bfb3b69e5d34c660
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-26 15:20:18 -0300

    Desafio 004

---
+++ DesafiosModulo01/src/Desafios/des004.kt
@@ -0,0 +1,17 @@
+package Desafios
+
+import kotlin.math.pow
+
+fun main() {
+    println("""${VERMELHO}----------------------------------
+       CÁLCULO DE POTÊNCIA
+----------------------------------${RECET}""")
+    print("Base: ")
+    val b:Short = readln().toShort()
+    print("Expoente: ")
+    val e:Short = readln().toShort()
+    print("""${AMALERO}------------RESULTADO-------------
+Cálculando ${AZUL}$b ${AMALERO}elevado a ${AZUL}$e${AMALERO}
+O resultado é ${b.toFloat().pow(e.toFloat())}
+----------------------------------${RECET}""")
+}
