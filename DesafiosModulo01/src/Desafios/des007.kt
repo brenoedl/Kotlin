7916e5cccd4f50d8de7ecdf437ec2a0db83004ad
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-27 13:38:56 -0300

    Desafio 007

---
+++ DesafiosModulo01/src/Desafios/des007.kt
@@ -0,0 +1,18 @@
+package Desafios
+
+import kotlin.math.abs
+
+fun main() {
+    println("""${VERMELHO}----------------------------------
+        TENTE ADIVINHAR
+----------------------------------
+${AZUL}Vou "pessar" em um número entre 0..5${RECET}""")
+    val num:Byte = (0..5).random().toByte()
+    print("Seu palpite: ")
+    val palp:Byte = readln().toByte()
+    print("""${AMALERO}------------RESULTADO-------------
+Você disse que seria o valor${AZUL} $palp ${AMALERO}
+Eu penssei no número $num
+A diferrença foi de ${MAGENTA}${abs(num - palp)}
+${AMALERO}----------------------------------${RECET}""")
+}
