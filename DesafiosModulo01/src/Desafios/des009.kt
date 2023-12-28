606813ff1ac2d07a4e7ba71f6515b9da0cd708e6
Breno170 <96654144+brenoedl@users.noreply.github.com>
2023-12-28 10:47:51 -0300

    Desafio 009

---
+++ DesafiosModulo01/src/Desafios/des009.kt
@@ -0,0 +1,16 @@
+package Desafios
+import java.time.LocalDateTime
+fun main() {
+    val dh:LocalDateTime = LocalDateTime.now()
+    println("""${VERMELHO}----------------------------------
+            DATA E HORA
+----------------------------------${RECET}""")
+    print("""${AMALERO}------------RESULTADO-------------
+Ano:                ${dh.year}
+Mês:                ${dh.monthValue}
+Dia:                ${dh.dayOfMonth}
+Hora:               ${dh.hour}
+Minuto:             ${dh.minute}
+Segundo:            ${dh.second}
+----------------------------------${RECET}""")
+}
