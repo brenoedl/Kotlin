2f6f1a3266e2b562f0747c1d555254c989fb537b
Breno170 <96654144+brenoedl@users.noreply.github.com>
2024-01-07 11:56:01 -0300

    Update des003.kt

--- DesafiosModulo01/src/Desafios/des003.kt
+++ DesafiosModulo01/src/Desafios/des003.kt
@@ -4,10 +4,12 @@ fun main(args: Array<String>) {
     println("""${VERMELHO}-----------------------------------
         NOTAS ESTUDONALTA
 ----------------------------------${RECET}""")
+
     print("Nota 1: ")
     val n1:Float = readln().toFloat()
     print("Nota 2: ")
     val n2:Float = readln().toFloat()
+
     print("""${AMALERO}------------RESULTADO-------------
 As notas do aluno foram $n1 e $n2
 A média final foi ${(n1 + n2) / 2}
