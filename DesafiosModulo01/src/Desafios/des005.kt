4d4d5b20fa2f213abd736ccd15b41f429eb689e6
Breno170 <96654144+brenoedl@users.noreply.github.com>
2024-01-07 12:38:05 -0300

    Update des005.kt

--- DesafiosModulo01/src/Desafios/des005.kt
+++ DesafiosModulo01/src/Desafios/des005.kt
@@ -1,16 +1,22 @@
 package Desafios
 
+import java.text.NumberFormat
+
 fun main() {
+    val formato:NumberFormat = NumberFormat.getCurrencyInstance()
+
     println("""${VERMELHO}----------------------------------
         REAJUSTE SALARIAL
 ----------------------------------${RECET}""")
+
     print("Salario: R$")
     val sal:Float = readln().toFloat()
     print("Reajuste: (%) ")
     val reaj:Float = readln().toFloat()
+
     print("""${AMALERO}------------RESULTADO-------------
-O salário anterior era  R$$sal
+O salário anterior era  ${formato.format(sal)}
 O aumento será de       $reaj%
-E passará a ganhar      R$${sal * reaj / 100 + sal}
+E passará a ganhar      ${formato.format(sal * reaj / 100 + sal)}
 ----------------------------------${RECET}""")
 }
