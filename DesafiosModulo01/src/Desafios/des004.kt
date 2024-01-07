492cba15fe28ad8061f7044f8f330b75a20d3816
Breno170 <96654144+brenoedl@users.noreply.github.com>
2024-01-07 12:00:16 -0300

    Update des004.kt

--- DesafiosModulo01/src/Desafios/des004.kt
+++ DesafiosModulo01/src/Desafios/des004.kt
@@ -6,10 +6,12 @@ fun main() {
     println("""${VERMELHO}----------------------------------
        CÁLCULO DE POTÊNCIA
 ----------------------------------${RECET}""")
+
     print("Base: ")
     val b:Short = readln().toShort()
     print("Expoente: ")
     val e:Short = readln().toShort()
+
     print("""${AMALERO}------------RESULTADO-------------
 Cálculando ${AZUL}$b ${AMALERO}elevado a ${AZUL}$e${AMALERO}
 O resultado é ${b.toFloat().pow(e.toFloat())}
